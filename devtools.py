9b5388ec92f0246fa4a9216464568b162ef3f21b
Tomi Mäkinen <thompso0@hotmail.com>
2016-02-24 15:46:33 +0200

    Second revision of map_editor. To be continued.
    MapData will have it's own code for drawing the map. Layers(on, off), NPC-editor, etc.

--- devtools.py
+++ devtools.py
@@ -5,37 +5,64 @@ import sys
 
 
 class Map_editor:
+    self.tile_cursor_pos = (0, 0)
+    self.world_window_pos = (0, 0)
+    
+    self.tile_size = (64, 64)
+    self.world_window_tiles = (15, 15)
+    self.world_window_size = (0, 0)
+    
+    self.tile_picker = None
+    
     def __init__(self, resource_loader_inst):
         assert isinstance(resource_loader_inst, resources.Resource_Loader)
 
         self.tilesheet = resource_loader_inst.get_tilesheet
-
-    def draw_tilesheet_window(self, tilesheet_surface):
-
-        # Calculate how many tiles can fit in one row. tile_size 64
-        tilesheet_size = tilesheet_surface.get_size()
-        tiles_in_row = tilesheet_size[0] / 64
-        space_between_tiles = (tilesheet_size[0] % 64) / (tiles_in_row - 1)
-
-        # Print the tile to layer by looping the dict
-        index_of_tile_in_row = 1
-        row_index = 0
-        for key, value in self.tilesheet.items():
-
-            tile_x_position = (index_of_tile_in_row - 1) * 64
-            tile_y_position = row_index * 64
-            if index_of_tile_in_row > 1:
-                tile_x_position += space_between_tiles
-            # Value [0] surface, value[1] passability
-            tilesheet_surface.blit(value[0], (tile_x_position, tile_y_position))
-
-            if index_of_tile_in_row == tiles_in_row:
-                index_of_tile_in_row = 1
-                row_index += 1
-
-            index_of_tile_in_row += 1
-
-        return tilesheet_surface
+        
+        self.tile_picker = TilePicker((385, 840), self.tile_size)
+        
+        self.tile_cursor_pos = (0, 0)
+        x, y = self.world_window_tiles
+        sx, sy = self.tile_size
+        self.world_window_size = (x * sx, y * sy)
+        
+    class TilePicker():
+        self.surface = None
+        def __init__(self, surface_size, tile_size, tilesheet):
+    
+            # Calculate how many tiles we can fit in one row of TilePicker.
+            self.surface = pygame.Surface(surface_size)
+            self.surface_size = self.surface.get_size()
+            
+            self.tilesheet = tilesheet
+            
+            self.tiles_in_row = self.surface_size[0] / self.tile_size[0]
+            self.space_between_tiles = (self.surface_size[0] % self.tile_size[0]) / (self.tiles_in_row - 1)
+    
+            # Print the tile to layer by looping the dict
+            index_of_tile_in_row = 1
+            row_index = 0
+            for key, value in self.tilesheet.items():
+    
+                tile_x_position = (index_of_tile_in_row - 1) * 64
+                tile_y_position = row_index * 64
+                if index_of_tile_in_row > 1:
+                    tile_x_position += self.space_between_tiles
+                # Value [0] surface, value[1] passability
+                self.surface.blit(value[0], (tile_x_position, tile_y_position))
+    
+                if index_of_tile_in_row == tiles_in_row:
+                    index_of_tile_in_row = 1
+                    row_index += 1
+    
+                index_of_tile_in_row += 1
+                
+    class WorldWindow():
+        surface = None
+        surface_size = (0,0)
+            def __init__(self, tiles, tile_size):
+                self.surface_size = tiles[0]+tile_size[0], tiles[1]+tile_size[1]
+                self.surface = pygame.Surface(self.surface_size)
 
     def launch(self, map_data, screen):
         world_window_size = (64 * 13, 64 * 13)
@@ -43,7 +70,6 @@ class Map_editor:
         # Calculate map size
         assert isinstance(map_data, map_stuff.MapData)
         map_size = map_data.get_map_size
-        map_screen_size = world_window_size  # tile_value_to_screen_value(map_size)
 
         # Load surfaces
         world_window_surface = pygame.Surface((64 * 13, 64 * 13))
@@ -53,12 +79,10 @@ class Map_editor:
 
         surfaces = [world_window_surface, floor_surface, inventory_surface, char_surface]
 
-        tilesheet_surface = pygame.Surface((385, 840))
+        tilesheet_surface = pygame.Surface()
         tilesheet_rect = tilesheet_surface.get_rect(topleft=(895, 0))
 
         tilesheet_surface = self.draw_tilesheet_window(tilesheet_surface)
-
-        tilecursor_pos = (1, 1)
 
         # Clear all the events.
         pygame.event.clear()
@@ -85,8 +109,20 @@ class Map_editor:
             screen.blit(tilesheet_surface, tilesheet_rect)
 
             pygame.display.flip()
-
+            
+    def cursor_move(direction_tuple):
+        new_position = self.tile_cursor_pos[0]+direction_tuple[0], self.tile_cursor_pos[1]+direction_tuple[1]
+        self.tile_cursor_pos = new_position
+            
+    def get_mouse_target():
+        mouse_position = pygame.mouse.get_pos()
+        
+        
 
 def mouse_position_to_caption():
     mouse_position = str(pygame.mouse.get_pos())
     pygame.display.set_caption(mouse_position)
+    
+    
+def mouse_position_to_tile():
+    mouse_position = pygame.mouse.get_pos()
